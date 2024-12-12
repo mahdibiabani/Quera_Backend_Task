49f916c51a5b0bbeaf7f1a9ccf613904fc74b0c9
mahdibiabani <156238864+mahdibiabani@users.noreply.github.com>
2024-12-13 00:15:44 +0330

    Create Form model

--- form/models.py
+++ form/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Form(models.Model):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
