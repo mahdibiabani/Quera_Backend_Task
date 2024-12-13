38bab0e6dd1f0481ca2a5a5951e3a1386f57939f
mahdibiabani <156238864+mahdibiabani@users.noreply.github.com>
2024-12-13 09:34:46 +0330

    Add viewset for managing forms and questions

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     #Custom apps
     'form',
 ]
@@ -124,3 +126,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
--- form/views.py
+++ form/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Form, Response
+from .serializers import FormSerializer, QuestionSerializer
 
-# Create your views here.
+
+class FormViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Form.objects.all()
+    serializer_class = FormSerializer
+    permission_classes = [permissions.AllowAny]  # Allow anyone to view forms
+
+    @action(detail=True, methods=['get'], url_path='questions')
+    def get_questions(self, request, pk=None):
+        form = self.get_object()
+        questions = form.questions.all()
+        serializer = QuestionSerializer(questions, many=True)
+        return Response(serializer.data)
