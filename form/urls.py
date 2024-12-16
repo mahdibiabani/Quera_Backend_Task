fa99c11c4c1d6af94b91a15a8ebc2fc938f16350
mahdibiabani <156238864+mahdibiabani@users.noreply.github.com>
2024-12-16 13:57:21 +0330

    Add tests for views

--- form/urls.py
+++ form/urls.py
@@ -8,6 +8,6 @@ router.register(r'forms', FormViewSet, basename='form')
 
 # Create a nested router for responses under forms
 forms_router = NestedDefaultRouter(router, r'forms', lookup='form')
-forms_router.register(r'responses', ResponseViewSet, basename='form-responses')
+forms_router.register(r'responses', ResponseViewSet, basename='response')
 
 urlpatterns = router.urls + forms_router.urls
